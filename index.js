08fc34eb35cf23fa381b0216b69173a4d235f974
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-11-10 13:03:22 -0300

    Hello size

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 console.log("index.js loaded");
-// require('dotenv').config();
-
 
 const port = 3000
 
@@ -8,26 +6,19 @@ const port = 3000
 document.addEventListener("DOMContentLoaded", function() {
     const contactForm = document.getElementById("contactForm");
 
-    // console.log("entrando a la función");
-
     contactForm.addEventListener("submit", function(event) {
         event.preventDefault();
-
-        // console.log("Form submitted");
 
         const name = document.getElementById("name").value;
         const email = document.getElementById("email").value;
         const message = document.getElementById("message").value;
 
-        // Objeto de datos a enviar al servidor Node.js
         const data = {
             name: name,
             email: email,
             message: message
         };
 
-        // console.log('data: ',data);
-        // Enviar los datos al servidor Node.js
         fetch(`http://127.0.0.1:3000/send-email`, {
             method: "POST",
             headers: {
@@ -36,19 +27,16 @@ document.addEventListener("DOMContentLoaded", function() {
             body: JSON.stringify(data),
         })
         .then(console.log('then after fetch'))
-        .then(response => response.json()) // Parsea la respuesta como JSON
+        .then(response => response.json())
         .then(data => {
             if (data.success) {
                 alert("Message sent successfully");
                 contactForm.reset();
             } else {
-                // console.log('inside fetch'),
                 alert("Error sending message. Please, try it again later.");
             }
         })
-        // .then(console.log('just before catch'));
         .catch(error => {
-            // console.log('inside fetch catch');
             alert("There was an error in the request. Please, try it again later.");
             console.log(error);
         });
